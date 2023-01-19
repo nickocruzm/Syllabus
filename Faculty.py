4d8649ad3529f1ac5ea1a0864bde776200aaebcd
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-18 23:19:45 -0800

    Constructors and update method

--- Faculty.py
+++ Faculty.py
@@ -1,7 +1,15 @@
+import Office
 class Faculty_Member:
-    def __init__(self):
-        pass
-    
+    def __init__(self, office: Office, name: str="", email: str=""):
+        self.Name   = name
+        self.Office = office
+        self.Email  = email
+
+    def update(self,attribute: str, x: str):
+        attr = getattr(self, attribute)
+        self.attr = x
+
+
 class Professor(Faculty_Member):
     def __init__(self):
         pass
