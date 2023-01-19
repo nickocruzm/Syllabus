c47ec7913e61c89b73384ea671a47dc7aaa9a936
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-18 23:20:54 -0800

    Office init

--- Office.py
+++ Office.py
@@ -1,3 +1,6 @@
 class Office:
-    def __init__(self):
-        pass
+    def __init__(self, hours, days, roomNum, building):
+        self.Location = {"RoomNumber": roomNum, "Building": building}
+        self.Days = days
+        self.Hours = hours
+    
