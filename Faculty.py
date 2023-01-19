0469cb18c9258259dd23807a2dbbbbf76f6c8da3
Nicko Cruz M <nickocruzm@gmail.com>
2023-01-18 22:41:13 -0800

    Class Design start

---
+++ Faculty.py
@@ -0,0 +1,11 @@
+class Faculty_Member:
+    def __init__(self):
+        pass
+    
+class Professor(Faculty_Member):
+    def __init__(self):
+        pass
+
+class Teacher_Assistant(Faculty_Member):
+    def __init__(self):
+        pass
